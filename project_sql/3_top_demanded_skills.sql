fb9653c8b1409079c9f977c7a436c2cd5edf1bb2
Loveman Singh <lovemansingh10@gmail.com>
2025-01-25 01:43:34 +0530

    change down

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,10 @@
+select skills,
+    count(job_postings_fact.job_id) as totel_demand
+from job_postings_fact
+INNER JOIN skills_job_dim ON
+    job_postings_fact.job_id = skills_job_dim.job_id
+inner join skills_dim on 
+    skills_dim.skill_id = skills_job_dim.skill_id
+WHERE job_title_short LIKE 'Data Analyst'
+GROUP BY skills
+ORDER BY totel_demand DESC
---
+++ project_sql/4_top_paying_skills.sql
@@ -0,0 +1,11 @@
+select skills_dim.skills,
+    round(avg(salary_year_avg), 0) as avg_salary_per_skill
+from job_postings_fact
+INNER JOIN skills_job_dim ON
+    job_postings_fact.job_id = skills_job_dim.job_id
+inner join skills_dim on 
+    skills_dim.skill_id = skills_job_dim.skill_id
+WHERE job_title_short LIKE 'Data Analyst'
+    and salary_year_avg is not NULL
+GROUP BY skills
+ORDER BY avg_salary_per_skill DESC
